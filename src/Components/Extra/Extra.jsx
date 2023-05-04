854253a6cfc3ed6bc9f8781f4a6f387c690ea97e
Sakibur <21103090@iubat.edu>
2023-05-03 20:52:21 -0700

    add extra session

--- src/Components/Extra/Extra.jsx
+++ src/Components/Extra/Extra.jsx
@@ -3,43 +3,74 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const Extra = () => {
     return (
-        <div>
+        <div className='mt-4'>
+
+          <div>
+            <h2 className='text-center m-4'> Chinese Best Noodles</h2>
+          </div>
             <Container>
       <Row>
       <Col sm={8}>
-
-      <Container>
-          <Row>
-            
-         <Col className='mt-4'   md={4}>
-         <Container>
-          <Row>
-            
-         <Col className='mt-4'   md={4}>
-              <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+         <div className='card m-4'>
+      <Row>
+      <Col sm={8}>
+         
+      <img className='mt-4 mb-4' src={`https://i.ibb.co/GnvHTqP/download-7.jpg`} alt="" />
+      </Col>
+        <Col sm={4}>
+        <h4>Mixian</h4>
+        <p>China's Yunnan Province is known for its rice noodles, and this noodle soup dish is very popular there. It's a great mixture of textures and flavors––spicy, tangy, and savory all at once!</p>
+        <button className='btn btn-primary mb-2'>Buy now</button>
+          </Col>
+      </Row>
+      </div>
+         <div className='card m-4'>
+      <Row>
+      <Col sm={8}>
+         
+      <img className='mt-4 mb-4' src={`https://i.ibb.co/zHtJTrL/ae09eae7d55745b19059dbe883da3829.jpg`} alt="" />
+      </Col>
+        <Col sm={4}>
+        <h4>Dan Dan noodles</h4>
+        <p>The chili oil and peppercorns add much of the spice while the sui mi ya cai (preserved mustard greens) provides a balanced sweet</p>
+        <button className='btn btn-primary mb-2'>Buy now</button>
+          </Col>
+      </Row>
+      </div>
+         <div className='card m-4'>
+      <Row>
+      <Col sm={8}>
+         
+      <img className='mt-4 mb-4' src={`https://i.ibb.co/0nWsw14/download-8.jpg`} alt="" />
+      </Col>
+        <Col sm={4}>
+        <div>
+          <h4>Lo mein</h4>
+        <p> chow mein means fried noodles and lo mein translates to tossed or stirred noodles. Because both dishes are variations of noodles, the main difference in chow mein and lo mein lies in how the noodles are prepared.</p>
+        <button className='btn btn-primary mb-2'>Buy now</button>
+        </div>
+          </Col>
+      </Row>
+      </div>
+      </Col>
+        <Col sm={4}>
+        <div className='mt-4'>
+      
+        <Card style={{ width: '18rem' }}>
+        <Card.Title className='text-center text-info mb-6 mt-2'>The best chef In china</Card.Title>
+      <Card.Img className='m-2 img-fluid' variant="top" src={`https://i.ibb.co/K9XGYv5/images-3.jpg`} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title className='text-center text-info'>Name :Zhou Guizhen</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+         <p> Known as the "Noodle Master," Zhou has been making hand-pulled noodles for over 40 years. She owns a small noodle shop in northwest China and is famous for her incredible skill and speed in making noodles.</p>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">About More</Button>
       </Card.Body>
     </Card>
- </Col>
-
-          </Row>
-     </Container>
- </Col>
-
-          </Row>
-     </Container>
-
-      </Col>
-        <Col sm={4}>sm=4</Col>
+        </div>
+          </Col>
       </Row>
-      
+    
     </Container>
         </div>
     );
--- src/Components/Recipe/Recipes.jsx
+++ src/Components/Recipe/Recipes.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row, Table } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Recipes = ({recipe}) => {
   const notify = () => toast("Add to favorite");
 
-  const {name,image,details}=recipe;
+  const {name,image,rating,cookingMethod,ingredients}=recipe;
     console.log(recipe)
     return (
       <Col  className='mt-4'   md={4}>
@@ -15,13 +15,40 @@ const Recipes = ({recipe}) => {
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
-        <h5>{details}</h5>
+          
+          <Table striped bordered hover size="sm">
+          <thead>
+          <tr>
+          <th>ingredients</th>
+          </tr>
+      </thead>
+   <tbody>
+        <tr>
+          <td>{ingredients}</td>
+         </tr>
+      </tbody>
+    </Table>
+          <Table striped bordered hover size="sm">
+          <thead>
+          <tr>
+          <th>cookingMethod</th>
+          </tr>
+      </thead>
+   <tbody>
+        <tr>
+          <td>{cookingMethod}</td>
+         </tr>
+      </tbody>
+    </Table>
+      
+        <h5>Rating :{rating}</h5>
         </Card.Text>
         <Button variant="primary" onClick={notify}>Favorite</Button>
         <ToastContainer />
       </Card.Body>
       </Card>
         </Col>
+        
     );
 };
 
